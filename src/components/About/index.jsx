0aedbbcd9f79c5aa7b490d8f7f885c30d19e77c8
Maria <bouhrem.maria@gmail.com>
2025-09-14 00:49:10 +0100

    add about content

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,24 +2,50 @@ import React from "react";
 import Image from "next/image";
 const About = () => {
   return (
-    <section className=" m-5 md:m-10 lg:m-20">
+    <section className="relative my-10 p-5 md:p-10 lg:p-20 flex flex-col justify-start gap-16 lg:gap-24">
+      {" "}
+      <div className="relative  ">
+        <h1 className="text-primary-500 font-light text-heading-m  lg:text-[47px]  capitalize ">
+          {" "}
+          About <span className="text-secondary-500 uppercase">
+            cse
+          </span> club{" "}
+        </h1>
+        <div className="absolute top-7 w-72 md:w-[350px] lg:w-[450px] lg left-0 ">
+          <Image
+            src="/assets/Vector (1).svg"
+            alt="vector"
+            width={450}
+            height={100}
+          />
+        </div>
+      </div>
       <div>
-        <div className="relative  ">
-          <h1 className="text-primary-500 font-light text-heading-m md:text-4xl lg:text-5xl  capitalize ">
-            {" "}
-            About <span className="text-secondary-500 uppercase">
-              cse
-            </span> club{" "}
-          </h1>
-          <div className="absolute top-5 w-72 md:w-[350px] lg:w-[450px] lg left-0 ">
-            <Image
-              src="/assets/Vector (1).svg"
-              alt="vector"
-              width={450}
-              height={100}
-            />
+        <div className="text-neutral-950 flex flex-col justify-start gap-10  w-full lg:w-[50%] dark:text-neutral-50">
+          <p className="text-text-m md:text-text-l">
+            CSE—the Scientific Club of ESI! Founded in 2008 by students from the
+            National School of Computer Science, our mission is to enhance
+            student life through engaging learning experiences and the sharing
+            of knowledge in technology.
+          </p>
+          <div className="relative    w-full flex capitalize text-text-m rounded-full justify-evenly items-center  ">
+            <div className="bg-primary-100  py-5  w-full flex capitalize text-text-m rounded-full justify-evenly items-center  dark:bg-primary-800">
+              <span>+600 members</span>
+            </div>
+            <div className="h-11 absolute inset-0 m-auto  bg-primary-100 rounded-t-sm rounded-b-sm w-10 ">
+              <div className="relative flex flex-col items-center justify-between w-full h-full">
+                <span className=" absolute -top-3  h-5 w-5 rounded-full  "></span>
+                <span className="h-5 w-5 absolute -bottom-3  rounded-full "></span>
+              </div>
+            </div>
+            <div className="bg-primary-100   py-5 w-full flex capitalize text-text-m rounded-full justify-evenly items-center  dark:bg-primary-800">
+              <span>+20 partners</span>
+            </div>
           </div>
         </div>
+      </div>
+      <div className=" hidden lg:block absolute space-y-reverse -bottom-5 w-72 md:w-[280px] lg -left-1 ">
+        <Image src="/assets/Vector.svg" alt="vector" width={450} height={100} />
       </div>
     </section>
   );
